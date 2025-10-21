cb5c0ac4f33191b6db598acc69738dd46593c04a
e1b23020 <e1b23020@oit.ac.jp>
2025-10-21 15:14:32 +0900

    Sample4-1:sample41.htmlを表示するGETリクエストを実装する

---
+++ src/main/java/oit/is/team63/nsk3/teamsixthree/controller/Sample41Controller.java
@@ -0,0 +1,43 @@
+package oit.is.team63.nsk3.teamsixthree.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample4")
+public class Sample41Controller {
+
+
+  @GetMapping("step1")
+  public String sample41() {
+    return "sample41.html";
+  }
+
+  @GetMapping("step3")
+  public String sample43() {
+    return "sample43.html";
+  }
+
+  @GetMapping("step5")
+  public String sample45() {
+    return "sample45.html";
+  }
+
+  @GetMapping("step6")
+  public String sample46() {
+    return "sample46.html";
+  }
+
+  
+}
