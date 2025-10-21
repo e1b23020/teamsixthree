9bc7b001c7ecd98a706dd5acae5fee6b31e3a9de
e1b23020 <e1b23020@oit.ac.jp>
2025-10-21 13:54:23 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team63/nsk3/teamsixthree/TeamsixthreeApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team63.nsk3.teamsixthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsixthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsixthreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team63/nsk3/teamsixthree/TeamsixthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team63.nsk3.teamsixthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsixthreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
